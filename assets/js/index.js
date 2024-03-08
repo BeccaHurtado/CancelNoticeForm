31b22c55e25ceb15dc6ecebe9ba34f654f56ff5b
BeccaHurtado <rebeccah7.rh@gmail.com>
2024-03-07 22:51:31 -0700

    added cancel request title and office address

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,20 +23,27 @@ function getCancelNoticePDF() {
     
     '<div class="container text-left pt-4">' +
         '<div class="row">' +
-            '<div class="pt-5 col-md-12">' + date + '</div>' +
-            '<div class="pt-2 col-md-12">Policy Number: ' + policyNumber + '</div>' +
-            '<div class="pt-5 col-md-12">' + firstName + ' ' + middleName + ' ' + lastName + '</div>' +
 
-            '<div class="pt-3 pb-5">This letter is to inform you that I wish to cancel my insurance policy effective (' + date + '). I no longer need coverage. Please return any preimum to the following address:</div>' +
+            '<div class="pb-5 col-md-6">' + date + '</div>' +
+            '<div class="pb-5 col-md-6 fw-bold text-end">Policy Number: #' + policyNumber + '</div>' +
+            '<div class="col-md-12 fw-bold text-capitalize">Estrella Insurance 215</div>' +
+            '<div class="col-md-12 fw-bold text-capitalize">20 S Power Rd Suite 104</div>' +
+            '<div class="col-md-12 fw-bold text-capitalize">Mesa, AZ 85206</div>' +
+            '<div class="pt-5 pb-2 col-md-12 fw-bold fs-4">Cancel Request</div>' +
+            '<div class="col-md-12 fw-bold pt-3">' + firstName + ' ' + middleName + ' ' + lastName + '</div>' +
             '<div>' + streetAddress + '</div>' +
-            '<div>' + city + ', ' + state + ' ' + zip + '</div>' +
-
+            '<div>' + city + ', ' + state + ' ' + zip + '</div>' +  
             
-            '<div class="pt-5 border-bottom border-dark col-md-6" >' + firstName + ' ' + middleName + ' ' + lastName + '</div>' +
-            '<div class="col-md-1"></div>' +
-            '<div class="pt-5 col-md-5 border-bottom border-dark">' + date +'</div>' +
+            '<div class="pt-3 pb-5">This letter is to inform you that I wish to cancel my insurance policy effective (' + date + '). I no longer need coverage. Please return any preimum to the following address:</div>' +
+
+            '<div>' + firstName + ' ' + lastName + '</div>' +
+            '<div>' + date + '</div>' +
+
         '</div>' +
     '</div>'
+
+
+            
     
     ;
 
